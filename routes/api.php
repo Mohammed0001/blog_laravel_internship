583c9d1c8c764c0860c6d78183d06d6d7385ec38
Bakr <maboubakr9@gmail.com>
2024-08-11 21:42:20 +0300

    comments feature

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\UserController;
+use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -11,17 +12,20 @@ use Illuminate\Support\Facades\Route;
 // Route::put('/posts/{post}', [PostController::class, 'update']);
 // Route::delete('/posts/{post}', [PostController::class, 'destroy']);
 
-Route::apiResource('posts', PostController::class);
-Route::apiResource('users', UserController::class);
-
-Route::get('/userPosts/{userID}', [ PostController::class, 'showUserPosts']);
-
-// Route::get('/users/{user}/posts', [PostController::class, 'showUserPosts']);
-
-
 // Route::get( '/users', [UserController::class, 'index']);
 // Route::get('/users/{user}', [UserController::class, 'show']);
 // Route::post('/users' , [UserController::class , 'store']);
 // Route::put('/users/{user}', [UserController::class, 'update']);
 // Route::delete('/users/{user}', [UserController::class, 'destroy']);
+
+Route::apiResource('posts', PostController::class);
+Route::apiResource('users', UserController::class);
+
+// Route::apiResource('users.posts', PostController::class);
+
+
+Route::get('/userPosts/{userID}', [ PostController::class, 'showUserPosts']);
+
+// Route::get('/users/{user}/posts', [PostController::class, 'showUserPosts']);
+
 
